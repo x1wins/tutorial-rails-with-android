e9db07dd425f93e8e94d891a506ed202eb7e90e9
RhPlus <x1wins@changwoo.net>
2020-02-19 17:04:05 -0800

    Added CollapsingToolbarLayout title https://stackoverflow.com/a/41584503/1399891

--- app/src/main/java/org/changwoo/rhee/tutorial_post_android/ScrollingActivity.java
+++ app/src/main/java/org/changwoo/rhee/tutorial_post_android/ScrollingActivity.java
@@ -1,19 +1,24 @@
 package org.changwoo.rhee.tutorial_post_android;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import io.swagger.client.model.Auth;
+import io.swagger.client.model.Post;
 
 public class ScrollingActivity extends AppCompatActivity {
-
+    private Post mPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -24,5 +29,10 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mPost = (Post) getIntent().getSerializableExtra("post");
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolbarLayout.setTitle(mPost.getTitle());
+        TextView textView = findViewById(R.id.detail_content);
+        textView.setText(mPost.getBody());
     }
 }
