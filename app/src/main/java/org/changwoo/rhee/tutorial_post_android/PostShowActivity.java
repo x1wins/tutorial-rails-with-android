e560d514d28a4b187cb2103a84834a71dccbd041
RhPlus <x1wins@changwoo.net>
2020-02-24 11:29:55 -0800

    Added comment post

--- app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostShowActivity.java
+++ app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostShowActivity.java
@@ -1,32 +1,81 @@
 package org.changwoo.rhee.tutorial_post_android;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 import com.squareup.picasso.Picasso;
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.Configuration;
+import io.swagger.client.api.CommentApi;
+import io.swagger.client.auth.ApiKeyAuth;
+import io.swagger.client.model.Auth;
 import io.swagger.client.model.Comment;
+import io.swagger.client.model.CommentParam;
 import io.swagger.client.model.Post;
 
 import java.util.List;
 
 public class PostShowActivity extends AppCompatActivity {
+    private Auth mAuth;
     private Post mPost;
     private ListView mList;
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_show);
-        mList = (ListView)findViewById(R.id.post_show_list);
+        mAuth = (Auth) getIntent().getSerializableExtra("auth");
         mPost = (Post) getIntent().getSerializableExtra("post");
+        mList = (ListView)findViewById(R.id.post_show_list);
+        mEditText = (EditText) findViewById(R.id.edittext_chatbox);
+        mButton = (Button) findViewById(R.id.button_chatbox_send);
         getSupportActionBar().setTitle(mPost.getTitle());
         buildListView(mPost);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask<Auth, Void, Comment> asyncTask = new AsyncTask<Auth, Void, Comment>() {
+                    @Override
+                    protected Comment doInBackground(Auth... auth) {
+                        String authorization = auth[0].getToken();
+                        ApiClient defaultClient = Configuration.getDefaultApiClient();
+                        ApiKeyAuth Bearer = (ApiKeyAuth) defaultClient.getAuthentication("Bearer");
+                        Bearer.setApiKey(authorization);
+                        CommentApi apiInstance = new CommentApi();
+                        String message = mEditText.getEditableText().toString();
+                        Integer postId = mPost.getId();
+                        CommentParam body = new CommentParam(message, postId);
+                        try {
+                            Comment result = apiInstance.apiV1CommentsPost(body, authorization);
+                            Log.d(this.getClass().toString(), result.toString());
+                            return result;
+                        } catch (ApiException e) {
+                            Log.d(this.getClass().toString(),"Exception when calling CategoryApi#apiV1CategoriesGet");
+                            e.printStackTrace();
+                            return null;
+                        }
+                    }
+
+                    @Override
+                    protected void onPostExecute(Comment commentResponse) {
+                        super.onPostExecute(commentResponse);
+                        mPost.getComments().add(0, commentResponse);
+                        mEditText.setText("");
+                        mList.invalidateViews();
+                    }
+                };
+                asyncTask.execute(mAuth);
+
+            }
+        });
     }
 
     private void buildListView(final Post post){
