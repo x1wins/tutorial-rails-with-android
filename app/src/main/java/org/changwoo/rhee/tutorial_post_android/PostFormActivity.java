48c761e666882c8c7ce87cd233f208d4f64f9f44
RhPlus <x1wins@changwoo.net>
2020-02-24 15:23:40 -0800

    Added FormActivity

---
+++ app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostFormActivity.java
@@ -0,0 +1,13 @@
+package org.changwoo.rhee.tutorial_post_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_form);
+    }
+}
