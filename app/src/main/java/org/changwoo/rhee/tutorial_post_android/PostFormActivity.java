8f1fb79224dbdfd0a23e9220187558e12ea1e4c2
RhPlus <x1wins@changwoo.net>
2020-02-24 16:45:25 -0800

    Added menu

--- app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostFormActivity.java
+++ app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostFormActivity.java
@@ -1,7 +1,11 @@
 package org.changwoo.rhee.tutorial_post_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PostFormActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class PostFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_form);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_send) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
