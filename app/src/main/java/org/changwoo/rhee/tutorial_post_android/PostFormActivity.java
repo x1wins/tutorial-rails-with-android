824d8267554b61f360ebdd8c548fbd4618a2f402
Chang-Woo Rhee <changwoo.rhee@kooli.tv>
2020-02-24 16:53:26 -0800

    Added title

--- app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostFormActivity.java
+++ app/src/main/java/org/changwoo/rhee/tutorial_post_android/PostFormActivity.java
@@ -13,6 +13,7 @@ public class PostFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_form);
+        getSupportActionBar().setTitle("Form");
     }
 
     @Override
