be15e7e27b265b3711718d88113dcd0eb521834c
Changwoo Rhee <x1wins@changwoo.net>
2020-03-06 11:14:35 -0800

    Added https://rhee03.ngrok.io

--- app/src/main/java/io/swagger/client/ApiClient.java
+++ app/src/main/java/io/swagger/client/ApiClient.java
@@ -51,7 +51,8 @@ import io.swagger.client.auth.OAuth;
 
 public class ApiClient {
 
-    private String basePath = "http://10.0.2.2:3000";
+//    private String basePath = "http://10.0.2.2:3000";
+    private String basePath = "https://rhee03.ngrok.io";
     private boolean debugging = false;
     private Map<String, String> defaultHeaderMap = new HashMap<String, String>();
     private String tempFolderPath = null;
